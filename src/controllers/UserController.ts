268dd9ccf51bf72ee63611730709148de78498bc
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-12-30 23:52:19 +0100

    Add needed User-connected functionalities

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,62 @@
-import { Body, JsonController, OnUndefined, Post } from "routing-controllers";
+import _ from "lodash";
+import {
+  Body,
+  Get,
+  JsonController,
+  OnUndefined,
+  Param,
+  Post,
+  Put
+} from "routing-controllers";
+
+import User from "@models/User";
+import UserService from "@services/UserService";
 
 import { LoginRequest } from "LoginRequest";
-import UserService from "@services/UserService";
 
 @JsonController()
 export default class UserController {
   constructor(private userService = new UserService()) {}
+
+  @Get("/user/list")
+  @OnUndefined(400)
+  public getUsers() {
+    return this.userService.getUsers().then(users => {
+      if (!users) {
+        return undefined;
+      }
+      return users.map(user => _.omit(user.toJSON(), ["token", "password"]));
+    });
+  }
+
+  @Get("/user/single/:userId")
+  @OnUndefined(400)
+  public async getUser(@Param("userId") userId: number) {
+    return this.userService.getUserById(userId).then(user => {
+      if (!user) {
+        return undefined;
+      }
+      return _.omit(user.toJSON(), ["token", "password"]);
+    });
+  }
+
+  @Put("/user/:userId")
+  @OnUndefined(400)
+  public updateUser(
+    @Param("userId") userId: number,
+    @Body({ required: true }) newUserData: Partial<User>
+  ) {
+    return this.userService.updateUser(
+      userId,
+      _.omit(newUserData, [
+        "id",
+        "createdAt",
+        "updatedAt",
+        "deletedAt",
+        "token"
+      ])
+    );
+  }
 
   @Post("/user/login")
   @OnUndefined(401)
