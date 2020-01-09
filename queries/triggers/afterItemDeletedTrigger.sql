92fdbb9a157ec5ed36d603f756a23bef9a333531
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-01-09 13:59:45 +0100

    Fix triggers

--- queries/triggers/afterItemDeletedTrigger.sql
+++ queries/triggers/afterItemDeletedTrigger.sql
@@ -1,12 +1,14 @@
 CREATE or REPLACE FUNCTION after_item_deleted_trigger() RETURNS TRIGGER AS $$
 BEGIN
-    DELETE FROM game_owned_items WHERE item_id = NEW.id;
-    DELETE FROM game_offers WHERE item_id = NEW.id;
+    IF NEW.deleted_at IS NOT NULL THEN
+        UPDATE game_owned_items SET deleted_at = NOW() WHERE item_id = NEW.id;
+        UPDATE game_offers SET deleted_at = NOW() WHERE item_id = NEW.id;
+    END IF;
     RETURN NEW;
 END
 $$LANGUAGE 'plpgsql';
 
 DROP TRIGGER IF EXISTS trig_item_deleted on game_items;
 
-CREATE TRIGGER trig_item_deleted AFTER DELETE ON game_items FOR EACH ROW
+CREATE TRIGGER trig_item_deleted AFTER UPDATE ON game_items FOR EACH ROW
 EXECUTE PROCEDURE after_item_deleted_trigger();
